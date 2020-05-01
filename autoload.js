38bd18b7027148e2a963043688b6e684c50cece2
sffanmoe <64340689+sffanmoe@users.noreply.github.com>
2020-05-01 17:06:53 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://github.com/fghrsh/live2d_api/tree/1.0.1/"
+			apiPath: "https://github.com/fghrsh/live2d_api/tree/1.0.1/",
+			//cdnPath: "https://github.com/fghrsh/live2d_api/tree/1.0.1/"
 		});
 	});
 }
